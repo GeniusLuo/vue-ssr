7bee0fcb751d8542b59df752ab2cbe09b646bc49
GeniusLuo <505101092@qq.com>
2021-07-23 17:40:10 +0800

    ssr

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+
+export default {
+  name: "",
+  components: {
+    Hello,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">hello world</div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
